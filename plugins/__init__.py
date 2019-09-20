f90e41de0259069c94e88728dc8a5905a57d93d3
Scott Fitzgerald <sfitzgerald@eero.com>
2019-09-20 03:53:01 -0500

    fix imports and dependency graph

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,8 +2,8 @@ from __future__ import division, absolute_import, print_function
 
 from airflow.plugins_manager import AirflowPlugin
 
-import plugins.operators
-import plugins.helpers
+import operators
+import helpers
 
 # Defining the plugin class
 class UdacityPlugin(AirflowPlugin):
