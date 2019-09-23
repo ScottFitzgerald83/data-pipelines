18b7caa8516d9a9d0680ca03a25fdbd2b39af90e
Scott Fitzgerald <sfitzgerald@eero.com>
2019-09-23 00:16:24 -0500

    finish building out dag

--- dags/sparkify_etl.py
+++ dags/sparkify_etl.py
@@ -1,18 +1,22 @@
 from datetime import datetime, timedelta
-import os
 
 from airflow import DAG
+from airflow.hooks.base_hook import BaseHook
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.postgres_operator import PostgresOperator
 
-from airflow.operators import (DataQualityOperator, LoadDimensionOperator, StageToRedshiftOperator, LoadFactOperator)
+from airflow.operators import DataQualityOperator, LoadDimensionOperator, LoadFactOperator, StageToRedshiftOperator
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+IAM_ROLE = BaseHook.get_connection("redshift").extra_dejson.get('iam_role')
+S3_BUCKET = 'udacity-dend'
+LOG_KEY = 'log_data'
+SONG_KEY = 'song_data'
 
 default_args = {
     'owner': 'scott',
-    'start_date': datetime(2019, 9, 20),
+    'start_date': datetime(2019, 9, 22),
     'depends_on_past': False,
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
@@ -22,47 +26,83 @@ default_args = {
     'max_active_runs': 1
 }
 
-with DAG('sparkify_etl_dag',
+with DAG('sparkify_etl_dag_v002',
          default_args=default_args,
          description='S3 -> Redshift ETL for Sparkify songs and event data',
-         schedule_interval='@hourly') as dag:
+         template_searchpath='/usr/local/airflow',
+         schedule_interval='@daily') as dag:
 
     start_operator = DummyOperator(
-        task_id='Begin_execution')
+        task_id='Begin_execution'
+    )
+
+    create_tables_task = PostgresOperator(
+        task_id='create_tables',
+        params={"conn_id": "redshift"},
+        postgres_conn_id='redshift',
+        sql='plugins/helpers/create_tables.sql'
+    )
 
     stage_events_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_events')
+        task_id='Stage_events',
+        conn_id='redshift',
+        iam_role=IAM_ROLE,
+        s3_bucket=S3_BUCKET,
+        s3_key=LOG_KEY,
+        table="events_stage"
+    )
 
     stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_songs'
+        task_id='Stage_songs',
+        conn_id='redshift',
+        iam_role=IAM_ROLE,
+        s3_bucket=S3_BUCKET,
+        s3_key=SONG_KEY,
+        table="songs_stage"
     )
 
     load_songplays_table = LoadFactOperator(
-        task_id='Load_songplays_fact_table'
+        task_id='Load_songplays_fact_table',
+        conn_id="redshift",
+        sql=SqlQueries.songplay_table_insert
     )
 
     load_user_dimension_table = LoadDimensionOperator(
-        task_id='Load_user_dim_table'
+        task_id='Load_user_dim_table',
+        conn_id="redshift",
+        sql=SqlQueries.user_table_insert
     )
 
     load_song_dimension_table = LoadDimensionOperator(
-        task_id='Load_song_dim_table'
+        task_id='Load_song_dim_table',
+        conn_id="redshift",
+        sql=SqlQueries.song_table_insert
     )
 
     load_artist_dimension_table = LoadDimensionOperator(
-        task_id='Load_artist_dim_table'
+        task_id='Load_artist_dim_table',
+        conn_id="redshift",
+        sql=SqlQueries.artist_table_insert
     )
 
     load_time_dimension_table = LoadDimensionOperator(
-        task_id='Load_time_dim_table'
+        task_id='Load_time_dim_table',
+        conn_id="redshift",
+        sql=SqlQueries.time_table_insert
     )
 
     run_quality_checks = DataQualityOperator(
-        task_id='Run_data_quality_checks'
+        task_id='Run_data_quality_checks',
+        conn_id="redshift"
     )
 
+    finish_operator = DummyOperator(
+        task_id='End_execution')
+
 start_operator \
+    >> create_tables_task \
     >> [stage_events_to_redshift, stage_songs_to_redshift] \
     >> load_songplays_table \
     >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] \
-    >> run_quality_checks
+    >> run_quality_checks \
+    >> finish_operator
