633d85757e4ce0e003a7042c0c50fe48d700661e
Scott Fitzgerald <sfitzgerald@eero.com>
2019-09-23 00:16:07 -0500

    implement custom operators

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -6,19 +6,22 @@ from airflow.utils.decorators import apply_defaults
 
 
 class DataQualityOperator(BaseOperator):
+    """The operator's main functionality is to receive one or more SQL based test cases along with the expected results
+    and execute the tests. For each the test, the test result and expected result needs to be checked and if there is
+    no match, the operator should raise an exception and the task should retry and fail eventually.
+
+    For example one test could be a SQL statement that checks if certain column contains NULL values by counting all the
+    rows that have NULL in the column. We do not want to have any NULLs so expected result would be 0 and the test would
+    compare the SQL statement's outcome to the expected result."""
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 conn_id="",
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = conn_id
 
     def execute(self, context):
         table = context["table"]
